6379d36c1688a0a89b8faae639ee996bf3a15bfc
Dmyto Herko <dmytroherko@gmail.com>
2021-09-06 23:11:43 +0300

    From object to seconds

--- src/App.tsx
+++ src/App.tsx
@@ -7,38 +7,14 @@ import styles from './App.module.css';
 
 function App() {
   const [isCounting, setCounting] = useState(false);
-  const [time, setTime] = useState({ s:0, m:0, h:0 });
+  const [timeInSeconds, setTimeInSeconds] = useState(0);
   const [intervalId, setIntervalId] = useState<SetStateAction<NodeJS.Timeout>>();
 
-  let updatedS = time.s, updatedM = time.m, updatedH = time.h;
-
-  const run = () => {
-    if(updatedM === 60) {
-      updatedH++;
-      updatedM = 0;
-    }
-
-    if(updatedS === 60) {
-      updatedM++;
-      updatedS = 0;
-    }
-    updatedS++;
-    setTime({ s:updatedS, m:updatedM, h:updatedH });
-  };
-
   const startTimer = () => {
-    setIntervalId(setInterval(run, 1000));
+    setIntervalId(setInterval(() => {
+      setTimeInSeconds(seconds => seconds + 1);
+    }, 1000));
   };
-
-  const resetTimer = () => {
-    if (typeof intervalId === 'number') {
-      clearInterval(intervalId);
-    }
-    setTime({s: 0, m: 0, h: 0});
-    updatedS = 0;
-    updatedH = 0;
-    updatedM = 0;
-  }
 
   const stopTimer = () => {
     if (typeof intervalId === 'number') {
@@ -46,13 +22,16 @@ function App() {
     }
   }
 
+  const resetTimer = () => {
+    stopTimer();
+    setTimeInSeconds(0);
+  }
+
   return (
     <div className={styles.timerContainer}>
       <h1 className={styles.title}>ITOP1000 Timer</h1>
       <Dashboard
-        h={time.h}
-        m={time.m}
-        s={time.s}
+        timeInSeconds={timeInSeconds}
       />
       <div className={styles.buttonsWrapper}>
         <ButtonStartStop
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import styles from "./Dashboard.module.css";
 
 interface DashboardProps {
-  h: number;
-  m: number;
-  s: number;
+  timeInSeconds: number;
 }
 
-export const Dashboard = (props: DashboardProps) => {
-  const { h, m, s } = props;
+export const Dashboard = ({timeInSeconds}: DashboardProps) => {
+  const hours = Math.floor(timeInSeconds / 3600);
+  const minutes = Math.floor((timeInSeconds - hours * 3600) / 60);
+  const seconds = Math.floor(timeInSeconds - hours * 3600 - minutes * 60);
+
   return (
     <div className={styles.container}>
-      <div className={styles.card}>{h < 10 ? '0' + h : h}</div>
+      <div className={styles.card}>{hours < 10 ? '0' + hours : hours}</div>
       :
-      <div className={styles.card}>{m < 10 ? '0' + m : m}</div>
+      <div className={styles.card}>{minutes < 10 ? '0' + minutes : minutes}</div>
       :
-      <div className={styles.card}>{s < 10 ? '0' + s : s}</div>
+      <div className={styles.card}>{seconds < 10 ? '0' + seconds : seconds}</div>
     </div>
   );
 };
